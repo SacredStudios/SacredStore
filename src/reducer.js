f256e21a71014a6ccb80035b290b8009a1b58bfa
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2025-01-26 03:15:22 -0500

    changes

--- src/reducer.js
+++ src/reducer.js
@@ -14,6 +14,17 @@ const reducer = (state, action) => {
                 basket: [...state.basket, action.item],
             };
         case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id == action.id
+            );
+
+            let newBasket = [...state.basket];
+            if (index >= 0) {
+                newBasket
+            }
+            else {
+                console.warn(`Can't remove product (id: ${action.id}) is not in cart`);
+            }
             return {
                 ...state,
                 basket: [...state.basket, action.item],
