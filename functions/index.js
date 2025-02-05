0d7f797e1d4ceaedfa6d5b41fe2a603cbdbd7dac
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2025-02-05 16:01:54 -0500

    backend is running

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("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");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// App config
+const app = express();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world!");
+});
+
+// Exports (v2 style)
+exports.api = onRequest(app);
