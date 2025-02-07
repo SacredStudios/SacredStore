84ad50a7f1f67bdfcb9706e28855630c28cce2d9
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2025-02-07 00:46:33 -0500

    deployment works except on my mac safari for some reason

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,15 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
+const functions = require("firebase-functions");
 const stripe = require("stripe")(functions.config().stripe.secret_key);
 
 // App config
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 // API routes
@@ -16,28 +17,28 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello world!");
 });
 
-app.post('/payments/create', async (request, response) => {
-    try {
-      const total = request.query.total;
-      console.log("Received total:", total);
-  
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: "usd",
-      });
-  
-      // Log the entire paymentIntent
-      //console.log("PaymentIntent object:", paymentIntent);
-  
-      //console.log("Sending back:", paymentIntent.client_secret);
-      response.status(201).send({
-        clientSecret: paymentIntent.client_secret, // underscores are required from Stripe
-      });
-    } catch (error) {
-      console.error("Error creating PaymentIntent:", error);
-      response.status(500).send({ error: error.message });
-    }
-  });
-  
+app.post("/payments/create", async (request, response) => {
+  try {
+    const total = request.query.total;
+    console.log("Received total:", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+
+    // Log the entire paymentIntent
+    // console.log("PaymentIntent object:", paymentIntent);
+
+    // console.log("Sending back:", paymentIntent.client_secret);
+    response.status(201).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    console.error("Error creating PaymentIntent:", error);
+    response.status(500).send({error: error.message});
+  }
+});
+
 // Exports (v2 style)
 exports.api = onRequest(app);
